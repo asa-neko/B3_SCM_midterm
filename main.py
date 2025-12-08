25d9f8c0b4dd4e370349c7c5ace16ac01be9269f
asagi-neko <a23.5p5s@g.chuo-u.ac.jp>
2025-12-08 15:54:00 +0900

    モデル評価プログラムを追記

--- main.py
+++ main.py
@@ -22,6 +22,29 @@ df.info()
 
 print(df.describe().T)
 
+subset_for_stats = df[df['y'] > 0]['y']
+Q1 = subset_for_stats.quantile(0.25)
+Q3 = subset_for_stats.quantile(0.75)
+IQR = Q3 - Q1
+
+# 上限・下限の設定 (1.5倍)
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+# 数値的な外れ値候補 (通常の範囲を超えているか)
+is_numeric_outlier = (df['y'] < lower_bound) | (df['y'] > upper_bound)
+
+# 除外(保護)条件: イベント(1) または 売上が0
+# weatherやbargainが 1 のとき、または y が 0 のときは True
+is_protected = (df['Weather'] == 1) | (df['Bargain'] == 1) | (df['y'] == 0)
+
+# 「数値的に外れ値」かつ「保護対象ではない」ものを抽出
+final_outliers_mask = is_numeric_outlier & (~is_protected)
+
+# 該当する行を確認
+print("--- 除去対象となる外れ値データ ---")
+print(df[final_outliers_mask])
+
 # 時系列プロット
 lb.plot_line(df['ds'], df['y'], "timeline", "data")
 
@@ -62,3 +85,6 @@ events = pd.concat((h1,h2,h3))
 
 # イベント効果のみ考慮した需要予測
 lb.prediction(df, True, True, events, 365)
+
+# モデル評価
+lb.model_eva(df, True, True, events, 365)
